31af6dc574c4194c0afecd71a808e3b0cd92eddc
techminer <tech.miner95@gmail.com>
2024-12-07 03:14:39 +0900

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18", // Ensure the version is correct
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
